6946f9bc46135517d69bd16ea1f13e44cd515009
Shahnaaz Rahamatullah <shahnaaz.rahamatullah@jugo.io>
2023-10-22 21:35:41 +0100

    feat: rest_parameters.js

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,9 +1,8 @@
 // Instructions can be found in rest_parameters.md
 
 // Add rest parameters here!
-export function add(a, b) {
-  // Add a loop here
-  return a + b;
+export function add(...args) {
+  return args.reduce((total, currentValue) => total + currentValue, 0);
 }
 
-add(1, 2, 3, 4, 5);
+console.log(add(1, 2, 3, 4, 5));
