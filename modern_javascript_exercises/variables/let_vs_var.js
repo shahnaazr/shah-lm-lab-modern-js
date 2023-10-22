ce7dfcc9d0eb79ff49f89e6e5eaa0faee6c4b8b7
Shahnaaz Rahamatullah <shahnaaz.rahamatullah@jugo.io>
2023-10-22 20:46:37 +0100

    feat: let_vs_var.js

--- modern_javascript_exercises/variables/let_vs_var.js
+++ modern_javascript_exercises/variables/let_vs_var.js
@@ -1,19 +1,19 @@
 // Instructions can be found in let_vs_var.md
 
 // Problems with var
-
+let i;
 function count() {
-  for (var i = 0; i < 5; i++) {
+  for (let i = 0; i < 5; i++) {
     console.log(i + " inside the forLoop");
   }
   console.log(i + " outside the forLoop");
 }
 
-// count(); // <---- uncomment me!
+count(); // <---- uncomment me!
 
 // Another problem with var is that we can overwrite it, like this:
 
-var greeting = "Hello 😘";
-var greeting = "Not today, pal! 😤";
+let greeting = "Hello 😘";
+//let greeting = "Not today, pal! 😤";
 
-// console.log(greeting) // <----- uncomment me!
+console.log(greeting); // <----- uncomment me!
